266b6bb717de2e4ce7e12c38290b8ccadffde9d9
Muditha <muditha.jayasooriya@dci-student.org>
2025-09-01 08:56:30 +0200

    change2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,10 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+
+           <li className="tracking-[-.01em]">
+            This a another change, Testing git fetch and git pull
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
